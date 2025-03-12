f3920bc4fa4235ea073c6894a0209cb7cd1e9b0e
danaeswart <241022@virtualwindow.co.za>
2025-03-12 18:35:24 +0200

    Wholy shit im donee yaayayay doughnut

---
+++ chartjs-task/src/components/DonutChart.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import './DonutChart.css'
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js/auto";
+
+
+
+
+
+const DonutChart = () => {
+    return (
+        <div className="chart-container">
+             <h1>Chart Data Title</h1>
+            <Doughnut
+            data={{
+                labels: ["Apples", "Bananas", "Cherries"],
+                datasets: [
+                  {
+                    data: [30, 50, 20],
+                    backgroundColor: [
+                        "#FF7E4F",
+                      "#E12532", // Soft yellow
+                      "#2D4718", // Soft blue
+                    ],
+                    borderColor: "transparent",
+                    borderWidth:0,
+                  },
+                ],
+                
+            }}
+            >
+
+            </Doughnut>
+
+        </div>
+       
+
+
+    );
+  };
+
+  export default DonutChart;
--- chartjs-task/src/components/Navbar.js
+++ chartjs-task/src/components/Navbar.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "#282c34", height:'100vh', width:'10%', position:'fixed' }}>
+    <nav style={{ padding: "10px", backgroundColor: "#282c34", height:'100vh', width:'10%', position:'fixed', display:"flex", flexDirection:"column" }}>
       <Link to="/" style={{ margin: "10px", color: "white", textDecoration: "none"}}>Dashboard</Link>
-      {/* <Link to="/about" style={{ margin: "10px", color: "white", textDecoration: "none" }}>About</Link>
-      <Link to="/contact" style={{ margin: "10px", color: "white", textDecoration: "none" }}>Contact</Link> */}
+      <Link to="/about" style={{ margin: "10px", color: "white", textDecoration: "none", }}>About</Link>
+     {/* <Link to="/contact" style={{ margin: "10px", color: "white", textDecoration: "none" }}>Contact</Link> */}
     </nav>
   );
 }
