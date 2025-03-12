3f82f1363d043ddeb78e432f8116a78f6c4c1e59
Onthatile-Lesufi <231262@virtualwindow.co.za>
2025-03-12 21:15:46 +0200

    Update README

--- chartjs-task/src/components/Home.js
+++ chartjs-task/src/components/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Radar from './Radarchart';
+import DonutChart from "./DonutChart";
+import BarChart from "./bar-chart";
 
 
 function Home() {
   return (
   <div>
     <Radar />
+    <DonutChart/>
+    <BarChart/>
   </div>
   );
 }
